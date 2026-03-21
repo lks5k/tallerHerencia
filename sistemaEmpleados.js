b01721b4fbdec1c76efd4e3f82bde11a3adb8d29
lks5k <lukas_enrique5k@hotmail.com>
2026-03-20 23:59:41 -0500

    Avances ejercicio 1

--- sistemaEmpleados.js
+++ sistemaEmpleados.js
@@ -40,3 +40,42 @@ console.log(empleado3.info());
 
 empleado3.desactivar();
 console.log(empleado3.info());
+
+class lider extends Empleado {
+    constructor(nombre, salario, equipo) {
+        super(nombre, "Líder de Equipo", salario);
+        this.equipo = equipo
+    }
+    presentarEquipo() {
+        return "Los integrantes de nuestro equipo son: " + this.equipo.join(", ");
+    }
+    agregarMiembro(nombre) {
+        this.equipo.push(nombre);
+        return nombre + " ha sido agregado al equipo de trabajo.";
+    }
+    info() {
+        return super.info() + ", el total de colaboradores en el equipo: " + this.equipo.length;
+    }
+}
+
+// Checkpoint
+
+let liderVentas = new lider("Mariano", 6000000, ["Marlon", "Luis", "Lukas"]);
+let liderProduccion = new lider("Lucia", 6500000, ["María", "José"]);
+
+console.log("salario anual de Marlon: $" + empleado1.calcularSalarioAnual());
+console.log(liderVentas.desactivar());
+console.log("El estado de Mariano es: " + (liderVentas.activo ? "Acticvo" : "Desactivado"));
+
+console.log(liderProduccion.agregarMiembro("Juan"));
+
+console.log(liderVentas.presentarEquipo());
+
+let empleado4 = new Empleado("Felipe", "Desarrollador", 3000000);
+let empleado5 = new Empleado("Sofia", "Desarrolladora", 3000000);
+let empleado6 = new Empleado("Andres", "Desarrollador", 3000000);
+let empleado7 = new Empleado("Camila", "Desarrolladora", 3000000);
+
+let liderTecnologia = new lider("Valentina", 7000000, ["Daniela", "Mario"]);
+let liderCompras = new lider("Santiago", 6500000, ["Laura", "Diego"]);
+
